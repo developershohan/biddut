26c9b2216bde93f70aa6a31360a1f7d3d28a3e85
Shohan <dev.shohanur@gmail.com>
2024-08-27 15:52:26 +0600

    first commit

---
+++ inc/common/finbest-scripts.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * biddut_scripts description
+ * @return [type] [description]
+ */
+function biddut_scripts() {
+
+    /**
+     * all css files
+    */ 
+
+    wp_enqueue_style( 'biddut-fonts', biddut_fonts_url(), array(), '1.0.0' );
+    if( is_rtl() ){
+        wp_enqueue_style( 'bootstrap-rtl', BIDDUT_THEME_CSS_DIR.'bootstrap.rtl.min.css', array() );
+    }else{
+        wp_enqueue_style( 'bootstrap', BIDDUT_THEME_CSS_DIR.'bootstrap.css', array() );
+    }
+    wp_enqueue_style( 'animate', BIDDUT_THEME_CSS_DIR . 'animate.css', [] );
+    wp_enqueue_style( 'swiper-bundle', BIDDUT_THEME_CSS_DIR . 'swiper-bundle.css', [] );
+    wp_enqueue_style( 'magnific-popup', BIDDUT_THEME_CSS_DIR . 'magnific-popup.css', [] );
+    wp_enqueue_style( 'font-awesome-pro', BIDDUT_THEME_CSS_DIR . 'font-awesome-pro.css', [] );
+    wp_enqueue_style( 'flaticon', BIDDUT_THEME_CSS_DIR . 'flaticon.css', [] );
+    wp_enqueue_style( 'spacing', BIDDUT_THEME_CSS_DIR . 'spacing.css', [] );
+    wp_enqueue_style( 'biddut-core', BIDDUT_THEME_CSS_DIR . 'biddut-core.css', [], time() );
+    wp_enqueue_style( 'biddut-unit', BIDDUT_THEME_CSS_DIR . 'biddut-unit.css', [], time() );
+    wp_enqueue_style( 'biddut-custom', BIDDUT_THEME_CSS_DIR . 'biddut-custom.css', [] );
+    wp_enqueue_style( 'biddut-style', get_stylesheet_uri() );
+
+
+    // all js
+    wp_enqueue_script( 'waypoints', BIDDUT_THEME_JS_DIR . 'waypoints.js', [ 'jquery' ], false, true );
+    wp_enqueue_script( 'bootstrap-bundle', BIDDUT_THEME_JS_DIR . 'bootstrap-bundle.js', [ 'jquery' ], '', true );
+    wp_enqueue_script( 'swiper-bundle', BIDDUT_THEME_JS_DIR . 'swiper-bundle.js', [ 'jquery' ], false, true );
+    wp_enqueue_script( 'jquery-appear', BIDDUT_THEME_JS_DIR . 'jquery-appear.js', [ 'jquery' ], '', true );
+    wp_enqueue_script( 'jquery-knob', BIDDUT_THEME_JS_DIR . 'jquery-knob.js', [ 'jquery' ], '', true );
+    wp_enqueue_script( 'magnific-popup', BIDDUT_THEME_JS_DIR . 'magnific-popup.js', [ 'jquery' ], '', true );
+    wp_enqueue_script( 'jquery-nice-select', BIDDUT_THEME_JS_DIR . 'nice-select.js', [ 'jquery' ], '', true );
+    wp_enqueue_script( 'purecounter', BIDDUT_THEME_JS_DIR . 'purecounter.js', [ 'jquery' ], false, true );
+    wp_enqueue_script( 'countdown', BIDDUT_THEME_JS_DIR . 'countdown.js', [ 'jquery' ], false, true );
+    wp_enqueue_script( 'wow', BIDDUT_THEME_JS_DIR . 'wow.js', [ 'jquery' ], false, true );
+    wp_enqueue_script( 'isotope-pkgd', BIDDUT_THEME_JS_DIR . 'isotope-pkgd.js', [ 'imagesloaded' ], false, true );
+    wp_enqueue_script( 'biddut-main', BIDDUT_THEME_JS_DIR . 'main.js', [ 'jquery' ], false, true );
+
+    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+        wp_enqueue_script( 'comment-reply' );
+    }
+}
+add_action( 'wp_enqueue_scripts', 'biddut_scripts' );
+
+
+/*
+Register Fonts
+ */
+function biddut_fonts_url() {
+    $font_url = '';
+
+    /*
+    Translators: If there are characters in your language that are not supported
+    by chosen font(s), translate this to 'off'. Do not translate into your own language.
+     */
+    if ( 'off' !== _x( 'on', 'Google font: on or off', 'biddut' ) ) {
+        $font_url = 'https://fonts.googleapis.com/css2?'. urlencode('family=Jost:wght@400;500;600;700;800;900&family=Kumbh+Sans:wght@400;500;600;700;800&display=swap');
+    }
+    return $font_url;
+}
